a946e4af316776896c2bbd13bfdd93caccbbddd7
Roqueheli <roqueheliarenas@gmail.com>
2024-11-20 18:19:27 -0300

    creating services page

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -42,10 +42,10 @@ const Services = (props: Props) => {
 
   return (
     <section className="flex items-center justify-center mb-16">
-      <div className="max-w-screen-xl mx-auto px-4 text-gray-600 md:px-8">
+      <div className="max-w-screen-xl mx-auto px-4 md:px-8">
         <div className="relative max-w-2xl mx-auto sm:text-center">
           <div className="relative z-10">
-            <h3 className="text-gray-800 text-3xl font-semibold sm:text-4xl">
+            <h3 className="text-3xl font-semibold sm:text-4xl">
               Nuestros servicios
             </h3>
             <p className="mt-3 text-center">
@@ -60,12 +60,12 @@ const Services = (props: Props) => {
             {features.map((item, idx) => (
               <li
                 key={`services-${idx}`}
-                className="bg-white text-justify space-y-3 p-4 border rounded-lg"
+                className="text-justify space-y-3 p-4 border rounded-lg"
               >
                 <div className="text-yellow-600 pb-3">
                   <item.icon size={21} />
                 </div>
-                <h4 className="text-lg text-gray-800 font-semibold">
+                <h4 className="text-lg font-semibold">
                   {item.title}
                 </h4>
                 <p>{item.desc}</p>
